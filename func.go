25b1f52f14ea9e2175e514d0d8841bbe53d52235
Dante Catalfamo <dante.catalfamo@gmail.com>
2019-11-22 23:23:55 -0500

    indentGroups groups on tabs now too

--- func.go
+++ func.go
@@ -6,20 +6,21 @@ import (
 )
 
 func groupIndent(lines []string) [][]string {
-	var groups [][]string
 	var group []string
+	var groups [][]string
 
 	for _, line := range lines {
 		if len(line) == 0 {
 			continue
 		}
-		if []rune(line)[0] != ' ' {
+		firstChar := []rune(line)[0]
+		if firstChar == ' ' || firstChar == '\t' {
+			group = append(group, line)
+		} else {
 			if len(group) > 0 {
 				groups = append(groups, group)
 			}
 			group = nil
-			group = append(group, line)
-		} else {
 			group = append(group, line)
 		}
 	}
