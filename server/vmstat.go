b31e76d2640a0b98a7f45b42f9397e3ddb6c2c4c
Dante Catalfamo <dante.catalfamo@gmail.com>
2019-11-25 14:01:15 -0500

    begin vmstat struct

---
+++ server/vmstat.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"os/exec"
+	"strings"
+	"strconv"
+)
+
+type Vmstat struct {
+	Procs struct {
+		Running int `json:"running"`
+		Sleeping int `json:"sleeping"`
+	} `json:"procs"`
+	Memory struct {
+		Active int `json:"active"`
+		Free int `json:"free"`
+	} `json:"memory"`
+	Page struct {
+		Faults int `json:"faults"`
+		Relcaims int `json:"reclaims"`
+		PagedOut int `json:"pagedOut"`
+		PagedIn int `json:"pagedIn"`
+		Freed int `json:"freed"`
+		Scanned int `json:"scanned"`
+	} `json:"page"`
+	Disks []struct {
+		Name string `json:"name"`
+		Tranfers int `json:"transfers"`
+	} `json:"disks"`
+	Traps struct {
+		Interrupts int `json:"interrupts"`
+		SystemCalls int `json:"systemCalls"`
+		ContextSwitch int `json:"contextSwitch"`
+	} `json:"traps"`
+	CPU struct {
+		User int `json:"user"`
+		System int `json:"system"`
+		Idle int `json:"idle"`
+	} `json:"cpu"`
+}
