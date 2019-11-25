11b2760d9617618c5c0bc360dcf901684475783e
Dante Catalfamo <dante.catalfamo@gmail.com>
2019-11-25 14:03:05 -0500

    gofmt

--- server/vmstat.go
+++ server/vmstat.go
@@ -2,39 +2,39 @@ package main
 
 import (
 	"os/exec"
-	"strings"
 	"strconv"
+	"strings"
 )
 
 type Vmstat struct {
 	Procs struct {
-		Running int `json:"running"`
+		Running  int `json:"running"`
 		Sleeping int `json:"sleeping"`
 	} `json:"procs"`
 	Memory struct {
 		Active int `json:"active"`
-		Free int `json:"free"`
+		Free   int `json:"free"`
 	} `json:"memory"`
 	Page struct {
-		Faults int `json:"faults"`
+		Faults   int `json:"faults"`
 		Relcaims int `json:"reclaims"`
 		PagedOut int `json:"pagedOut"`
-		PagedIn int `json:"pagedIn"`
-		Freed int `json:"freed"`
-		Scanned int `json:"scanned"`
+		PagedIn  int `json:"pagedIn"`
+		Freed    int `json:"freed"`
+		Scanned  int `json:"scanned"`
 	} `json:"page"`
 	Disks []struct {
-		Name string `json:"name"`
-		Tranfers int `json:"transfers"`
+		Name     string `json:"name"`
+		Tranfers int    `json:"transfers"`
 	} `json:"disks"`
 	Traps struct {
-		Interrupts int `json:"interrupts"`
-		SystemCalls int `json:"systemCalls"`
+		Interrupts    int `json:"interrupts"`
+		SystemCalls   int `json:"systemCalls"`
 		ContextSwitch int `json:"contextSwitch"`
 	} `json:"traps"`
 	CPU struct {
-		User int `json:"user"`
+		User   int `json:"user"`
 		System int `json:"system"`
-		Idle int `json:"idle"`
+		Idle   int `json:"idle"`
 	} `json:"cpu"`
 }
