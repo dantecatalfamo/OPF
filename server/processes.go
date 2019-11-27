8830e08a96cd2698f4b010a875b097966c27c2b0
Dante Catalfamo <dante.catalfamo@gmail.com>
2019-11-27 18:12:17 -0500

    gofmt

--- server/processes.go
+++ server/processes.go
@@ -2,31 +2,31 @@ package main
 
 import (
 	"os/exec"
-	"strings"
 	"strconv"
+	"strings"
 )
 
 type Process struct {
-	User string  `json:"user"`
-	Group string  `json:"group"`
-	PID int  `json:"pid"`
-	ParentPID int `json:"parentPid"`
-	Stat []string `json:"stat"`
-	PercentCPU float64 `json:"percentCPU"`
-	PercentMemory float64 `json:"percentMemory"`
-	VirtualMemorySize int `json:"virtualMemorySize"`
-	ResidentSetSize int `json:"residentSetSize"`
-	Nice int `json:"nice"`
-	Priority int `json:"priority"`
-	WaitChannel string `json:"waitChannel"`
-	Elapsed struct {
-		Days int `json:"days"`
+	User              string   `json:"user"`
+	Group             string   `json:"group"`
+	PID               int      `json:"pid"`
+	ParentPID         int      `json:"parentPid"`
+	Stat              []string `json:"stat"`
+	PercentCPU        float64  `json:"percentCPU"`
+	PercentMemory     float64  `json:"percentMemory"`
+	VirtualMemorySize int      `json:"virtualMemorySize"`
+	ResidentSetSize   int      `json:"residentSetSize"`
+	Nice              int      `json:"nice"`
+	Priority          int      `json:"priority"`
+	WaitChannel       string   `json:"waitChannel"`
+	Elapsed           struct {
+		Days int    `json:"days"`
 		Time string `json:"time"`
 	} `json:"elapsed"`
-	Started string `json:"started"`
-	Time string `json:"time"`
+	Started  string `json:"started"`
+	Time     string `json:"time"`
 	Terminal string `json:"terminal"`
-	Command string `json:"command"`
+	Command  string `json:"command"`
 }
 
 func genProcess(line string, tz string) (*Process, error) {
@@ -49,24 +49,42 @@ func genProcess(line string, tz string) (*Process, error) {
 	statStr := fields[4]
 	for _, c := range statStr {
 		switch c {
-		case 'D': stat = append(stat, "uninterruptible")
-		case 'I': stat = append(stat, "idle")
-		case 'R': stat = append(stat, "runnable")
-		case 'S': stat = append(stat, "sleeping")
-		case 'T': stat = append(stat, "stopped")
-		case 'Z': stat = append(stat, "zombie")
-		case '+': stat = append(stat, "foreground")
-		case '<': stat = append(stat, "raised_priority")
-		case '>': stat = append(stat, "memory_limit_exceeded")
-		case 'E': stat = append(stat, "exiting")
-		case 'K': stat = append(stat, "kernel")
-		case 'N': stat = append(stat, "reduced_priority")
-		case 'p': stat = append(stat, "pledged")
-		case 's': stat = append(stat, "session_leader")
-		case 'U': stat = append(stat, "unveil_locked")
-		case 'u': stat = append(stat, "unveil_not_locked")
-		case 'V': stat = append(stat, "suspended_vfork")
-		case 'X': stat = append(stat, "debugging")
+		case 'D':
+			stat = append(stat, "uninterruptible")
+		case 'I':
+			stat = append(stat, "idle")
+		case 'R':
+			stat = append(stat, "runnable")
+		case 'S':
+			stat = append(stat, "sleeping")
+		case 'T':
+			stat = append(stat, "stopped")
+		case 'Z':
+			stat = append(stat, "zombie")
+		case '+':
+			stat = append(stat, "foreground")
+		case '<':
+			stat = append(stat, "raised_priority")
+		case '>':
+			stat = append(stat, "memory_limit_exceeded")
+		case 'E':
+			stat = append(stat, "exiting")
+		case 'K':
+			stat = append(stat, "kernel")
+		case 'N':
+			stat = append(stat, "reduced_priority")
+		case 'p':
+			stat = append(stat, "pledged")
+		case 's':
+			stat = append(stat, "session_leader")
+		case 'U':
+			stat = append(stat, "unveil_locked")
+		case 'u':
+			stat = append(stat, "unveil_not_locked")
+		case 'V':
+			stat = append(stat, "suspended_vfork")
+		case 'X':
+			stat = append(stat, "debugging")
 		}
 	}
 
