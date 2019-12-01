015b3029bd532580ca137be6b49a41f4cca17bc7
Dante Catalfamo <dante.catalfamo@gmail.com>
2019-12-01 00:44:14 -0500

    Some work on pf states

--- ui/src/Components/PfStates.js
+++ ui/src/Components/PfStates.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import { Tag, Table, Typography, Icon } from 'antd';
 import { getJSON } from '../helpers.js';
 import { serverURL } from '../config.js';
 import './PfStates.css';
 
+const { Text } = Typography;
+
 const pfStatesURL = `${serverURL}/api/pf-states`;
-const updateTime = 2000;
+const updateTime = 5000;
 
 function PfStates() {
   const [states, setStates] = useState([]);
@@ -19,6 +22,76 @@ function PfStates() {
       clearInterval(interval);
     };
   }, []);
+
+  const columns = [
+    {
+      title: "Protocol",
+      dataIndex: "proto",
+      width: "85px",
+      render: proto => {
+        let color;
+        if (proto === "tcp") { color = "blue"; }
+        if (proto === "udp") { color = "red"; }
+        return (<Tag color={color}>{proto.toUpperCase()}</Tag>);
+      }
+    },
+    {
+      title: "Direction",
+      dataIndex: "direction",
+      width: "76px",
+      render: direction => {
+        return (<span>{direction.toUpperCase()}</span>);
+      }
+    },
+    {
+      title: "Source",
+      dataIndex: "sourceIP",
+      width: "240px",
+      render: src => (<Text code>{src}</Text>)
+    },
+    {
+      title: "Destination",
+      dataIndex: "destinationIP",
+      width: "240px",
+      render: dst => (<Text code>{dst}</Text>)
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+      render: age => (<Text code>{age}</Text>)
+    },
+    {
+      title: "Expires",
+      dataIndex: "expires",
+      render: expr => (<Text code>{expr}</Text>)
+    },
+    {
+      title: "Source State",
+      dataIndex: "sourceState",
+    },
+    {
+      title: "Packets",
+      dataIndex: "packetsSent",
+    },
+    {
+      title: "Rule",
+      dataIndex: "rule",
+      render: rule => {
+        const num = rule === -1 ? "*" : rule;
+        return num;
+      }
+    }
+  ];
+
+  return (<Table
+            columns={columns}
+            dataSource={states}
+            pagination={false}
+            size="small"
+            rowKey="id"
+            expandedRowRender={row => <p style={{margin: 0}}>Gateway: {row.gateway}</p>}
+            scroll={{y: "85vh"}}
+          />);
 
   return (
     <div className="pfstates">
