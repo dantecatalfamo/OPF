584e44138c409ade4a9f88af289bad6b5eab2f9b
Dante Catalfamo <dante.catalfamo@gmail.com>
2019-12-01 21:34:55 -0500

    Begin rc page

--- ui/src/Components/RC.js
+++ ui/src/Components/RC.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { List, Button, Badge, Switch } from 'antd';
 import { getJSON } from '../helpers.js';
 import { serverURL } from '../config.js';
 import './RC.css';
@@ -6,6 +7,8 @@ import './RC.css';
 const rcAllURL = `${serverURL}/api/rc-all`;
 const rcOnURL = `${serverURL}/api/rc-on`;
 const rcStartedURL = `${serverURL}/api/rc-started`;
+
+const rcSpecials = ['pf', 'check_quotas', 'ipsec', 'accounting'];
 
 function RC() {
   const [rcAll, setRcAll] = useState([]);
@@ -17,6 +20,64 @@ function RC() {
     getJSON(rcOnURL).then(res => setRcOn(res));
     getJSON(rcStartedURL).then(res => setRcStarted(res));
   }, []);
+
+  return (
+    <List
+      itemLayout="horizontal"
+      dataSource={rcAll}
+      renderItem={item => {
+        let badge;
+        let started;
+        const special = rcSpecials.includes(item);
+        if (!rcStarted) {
+          badge = "processing";
+          started = null;
+        } else if (rcSpecials.includes(item)) {
+          badge = "default";
+        } else if (rcStarted.includes(item)) {
+          badge = "green";
+          started = true;
+        } else {
+          badge = "red";
+          started = false;
+        }
+        let enabled;
+        if (!rcOn) {
+          enabled = null;
+        } else if (rcOn.includes(item)) {
+          enabled = true;
+        } else {
+          enabled = false;
+        }
+
+        const startButton = (
+          <Button
+            loading={started == null}
+            type={started === true ? "danger" : "primary"}
+          >{started ? "Stop" : "Start"}</Button>
+        );
+
+        const enableSwitch = (
+          <Switch
+            loading={enabled === null}
+            checked={enabled}
+          >{enabled ? "Disable" : "Enable"}</Switch>
+        );
+
+        return (
+          <List.Item
+            actions={[
+              special ? null : startButton,
+              enableSwitch,
+            ]}>
+            <List.Item.Meta
+              title={<span><Badge status={badge} /> {item}</span>}
+            />
+          </List.Item>
+               );}}
+    />
+
+  );
 
   return (
     <div className="rc">
