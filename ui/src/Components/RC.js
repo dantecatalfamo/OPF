685099032398ec13346bae3b0e310f3f7a725275
Dante Catalfamo <dante.catalfamo@gmail.com>
2019-12-03 21:30:34 -0500

    wrap rc in a card

--- ui/src/Components/RC.js
+++ ui/src/Components/RC.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { List, Button, Badge, Switch, Col } from 'antd';
+import { List, Button, Badge, Switch, Col, Card } from 'antd';
 import { getJSON } from '../helpers.js';
 import { serverURL } from '../config.js';
 import RcFlags from './RcFlags';
@@ -23,76 +23,78 @@ function RC() {
   }, []);
 
   return (
-    <Col
-      xxl={{ span: 8,  offset: 8 }}
-      xl={{  span: 10, offset: 7 }}
-      lg={{  span: 12, offset: 6 }}
-    >
-      <List
-        itemLayout="horizontal"
-        bordered
-        dataSource={rcAll}
-        renderItem={item => {
-          let badge;
-          let started;
-          const special = rcSpecials.includes(item);
-          if (!rcStarted) {
-            badge = "processing";
-            started = null;
-          } else if (rcSpecials.includes(item)) {
-            badge = "default";
-          } else if (rcStarted.includes(item)) {
-            badge = "green";
-            started = true;
-          } else {
-            badge = "red";
-            started = false;
-          }
-          let enabled;
-          if (!rcOn) {
-            enabled = null;
-          } else if (rcOn.includes(item)) {
-            enabled = true;
-          } else {
-            enabled = false;
-          }
+    <Card>
+      <Col
+        xxl={{ span: 8,  offset: 8 }}
+        xl={{  span: 10, offset: 7 }}
+        lg={{  span: 12, offset: 6 }}
+      >
+        <List
+          itemLayout="horizontal"
+          bordered
+          dataSource={rcAll}
+          renderItem={item => {
+            let badge;
+            let started;
+            const special = rcSpecials.includes(item);
+            if (!rcStarted) {
+              badge = "processing";
+              started = null;
+            } else if (rcSpecials.includes(item)) {
+              badge = "default";
+            } else if (rcStarted.includes(item)) {
+              badge = "green";
+              started = true;
+            } else {
+              badge = "red";
+              started = false;
+            }
+            let enabled;
+            if (!rcOn) {
+              enabled = null;
+            } else if (rcOn.includes(item)) {
+              enabled = true;
+            } else {
+              enabled = false;
+            }
 
-          const startButton = (
-            <Button
-              loading={started == null}
-              type={started === true ? "danger" : "primary"}
-            >{started ? "Stop" : "Start"}</Button>
-          );
+            const startButton = (
+              <Button
+                loading={started == null}
+                type={started === true ? "danger" : "primary"}
+              >{started ? "Stop" : "Start"}</Button>
+            );
 
-          const enableSwitch = (
-            <Switch
-              loading={enabled === null}
-              checked={enabled}
-            >{enabled ? "Disable" : "Enable"}</Switch>
-          );
+            const enableSwitch = (
+              <Switch
+                loading={enabled === null}
+                checked={enabled}
+              >{enabled ? "Disable" : "Enable"}</Switch>
+            );
 
-          const flagsButton = (
-            <RcFlags
-              disabled={special || !enabled}
-              loading={!rcOn}
-              service={item}
-            />
-          );
-
-          return (
-            <List.Item
-              actions={[
-                special ? null : startButton,
-                enableSwitch,
-                flagsButton,
-              ]}>
-              <List.Item.Meta
-                title={<span><Badge status={badge} /> {item}</span>}
+            const flagsButton = (
+              <RcFlags
+                disabled={special || !enabled}
+                loading={!rcOn}
+                service={item}
               />
-            </List.Item>
-          );}}
-      />
-    </Col>
+            );
+
+            return (
+              <List.Item
+                actions={[
+                  special ? null : startButton,
+                  enableSwitch,
+                  flagsButton,
+                ]}>
+                <List.Item.Meta
+                  title={<span><Badge status={badge} /> {item}</span>}
+                />
+              </List.Item>
+            );}}
+        />
+      </Col>
+    </Card>
   );
 }
 
