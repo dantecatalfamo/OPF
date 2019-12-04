de43e15aab6c11550bb16c89f20426491dfb363b
Dante Catalfamo <dante.catalfamo@gmail.com>
2019-12-04 18:40:22 -0500

    Make info more aesthetic

--- ui/src/Components/PfInfo.js
+++ ui/src/Components/PfInfo.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Descriptions, Badge, Typography, Col, Card } from 'antd';
+import { Descriptions, Badge, Typography, Col, Row, Card, Statistic } from 'antd';
 import { getJSON } from '../helpers.js';
 import { serverURL } from '../config.js';
 
@@ -28,99 +28,106 @@ function PfInfo(props) {
   }
 
   return (
-    <Card>
       <Col xxl={{span: 16, offset: 4}} xl={{span: 20, offset: 2}} lg={{span: 24}}>
-        <Descriptions column={{xxl: 2, xl: 2, lg: 2, md: 1, sm: 1, xs: 1}} bordered>
-          <Item label="Status" span={2}>
-            <Badge status={pfInfo.status === "Enabled" ? "green" : "red" }/>
-            {pfInfo.status}
-          </Item>
-          <Item label="Since" span={2}>{pfInfo.since}</Item>
-          <Item label="Debug">{pfInfo.debug}</Item>
-          <Item label="Host ID">{pfInfo.hostId}</Item>
-          <Item label="Checksum" span={2}>{pfInfo.checksum}</Item>
+        <Card>
+          <Descriptions column={{xxl: 2, xl: 2, lg: 2, md: 1, sm: 1, xs: 1}} bordered>
+            <Item label="Status" span={2}>
+              <Badge status={pfInfo.status === "Enabled" ? "green" : "red" }/>
+              {pfInfo.status}
+            </Item>
+            <Item label="Since" span={2}>{pfInfo.since}</Item>
+            <Item label="Debug">{pfInfo.debug}</Item>
+            <Item label="Host ID">{pfInfo.hostId}</Item>
+            <Item label="Checksum" span={2}>{pfInfo.checksum}</Item>
+          </Descriptions>
+        </Card>
 
-          <Item label={<Text strong>State Table</Text>} span={4} />
-          <Item label="Current Entries">{pfInfo.stateTable.currentEntries}</Item>
-          <Item label="Half-Open TCP">{pfInfo.stateTable.halfOpenTcp}</Item>
-          <Item label="Total Searches">{pfInfo.stateTable.searches.total}</Item>
-          <Item label="Search Rate">{pfInfo.stateTable.searches.rate}/s</Item>
-          <Item label="Total Inserts">{pfInfo.stateTable.inserts.total}</Item>
-          <Item label="Insert Rate">{pfInfo.stateTable.inserts.rate}/s</Item>
-          <Item label="Total Removals">{pfInfo.stateTable.removals.total}</Item>
-          <Item label="Removal Rate">{pfInfo.stateTable.removals.rate}/s</Item>
+        <Card title="State Table">
+          <Row>
+            <Col span={6}><Statistic title="Current Entries" value={pfInfo.stateTable.currentEntries} /></Col>
+            <Col span={6}><Statistic title="Half-Open TCP" value={pfInfo.stateTable.halfOpenTcp} /></Col>
+            <Col span={6}><Statistic title="Total Searches" value={pfInfo.stateTable.searches.total} /></Col>
+            <Col span={6}><Statistic title="Search Rate" value={pfInfo.stateTable.searches.rate} suffix="/s" /></Col>
+            <Col span={6}><Statistic title="Total Inserts" value={pfInfo.stateTable.inserts.total} /></Col>
+            <Col span={6}><Statistic title="Insert Rate" value={pfInfo.stateTable.inserts.rate} suffix="/s" /></Col>
+            <Col span={6}><Statistic title="Total Removals" value={pfInfo.stateTable.removals.total} /></Col>
+            <Col span={6}><Statistic title="Removal Rate" value={pfInfo.stateTable.removals.rate} suffix="/s" /></Col>
+          </Row>
+        </Card>
 
-          <Item label={<Text strong>Source Tracking Table</Text>}span={4} />
-          <Item label="Current Entries" span={2}>{pfInfo.sourceTrackingTable.currentEntries}</Item>
-          <Item label="Total Searches">{pfInfo.sourceTrackingTable.searches.total}</Item>
-          <Item label="Search Rate">{pfInfo.sourceTrackingTable.searches.rate}/s</Item>
-          <Item label="Total Inserts">{pfInfo.sourceTrackingTable.inserts.total}</Item>
-          <Item label="Insert Rate">{pfInfo.sourceTrackingTable.inserts.rate}/s</Item>
-          <Item label="Total Removals">{pfInfo.sourceTrackingTable.removals.total}</Item>
-          <Item label="Removal Rate">{pfInfo.sourceTrackingTable.removals.rate}/s</Item>
+        <Card title="Source Tracking Table">
+          <Col span={12}><Statistic title="Current Entries" value={pfInfo.sourceTrackingTable.currentEntries} /></Col>
+          <Col span={6}><Statistic title="Total Searches" value={pfInfo.sourceTrackingTable.searches.total} /></Col>
+          <Col span={6}><Statistic title="Search Rate" value={pfInfo.sourceTrackingTable.searches.rate} suffix="/s" /></Col>
+          <Col span={6}><Statistic title="Total Inserts" value={pfInfo.sourceTrackingTable.inserts.total} /></Col>
+          <Col span={6}><Statistic title="Insert Rate" value={pfInfo.sourceTrackingTable.inserts.rate} suffix="/s" /></Col>
+          <Col span={6}><Statistic title="Total Removals" value={pfInfo.sourceTrackingTable.removals.total} /></Col>
+          <Col span={6}><Statistic title="Removal Rate" value={pfInfo.sourceTrackingTable.removals.rate} suffix="/s" /></Col>
+        </Card>
 
-          <Item label={<Text strong>Counters</Text>} span={4} />
-          <Item label="Total Matches">{pfInfo.counters.match.total}</Item>
-          <Item label="Match Rate">{pfInfo.counters.match.rate}/s</Item>
-          <Item label="Total Bad Offsets">{pfInfo.counters.badOffsets.total}</Item>
-          <Item label="Bad Offset Rate">{pfInfo.counters.badOffsets.rate}/s</Item>
-          <Item label="Total Fragments">{pfInfo.counters.fragments.total}</Item>
-          <Item label="Fragment Rate">{pfInfo.counters.fragments.rate}/s</Item>
-          <Item label="Total Shorts">{pfInfo.counters.shorts.total}</Item>
-          <Item label="Short Rate">{pfInfo.counters.shorts.rate}/s</Item>
-          <Item label="Total Normalizes">{pfInfo.counters.normalize.total}</Item>
-          <Item label="Normalize Rate">{pfInfo.counters.normalize.rate}/s</Item>
-          <Item label="Total Memory">{pfInfo.counters.match.total}</Item>
-          <Item label="Memory Rate">{pfInfo.counters.match.rate}/s</Item>
-          <Item label="Total Bad Timestamps">{pfInfo.counters.badTimestamp.total}</Item>
-          <Item label="Bad Timestamp Rate">{pfInfo.counters.badTimestamp.rate}/s</Item>
-          <Item label="Total Congestion">{pfInfo.counters.congestion.total}</Item>
-          <Item label="Congestion Rate">{pfInfo.counters.congestion.rate}/s</Item>
-          <Item label="Total IP Options">{pfInfo.counters.ipOption.total}</Item>
-          <Item label="IP Options Rate">{pfInfo.counters.ipOption.rate}/s</Item>
-          <Item label="Total Protocol Checksums">{pfInfo.counters.protoCksum.total}</Item>
-          <Item label="Protocol Checksum Rate">{pfInfo.counters.protoCksum.rate}/s</Item>
-          <Item label="Total State Mismatches">{pfInfo.counters.stateMismatch.total}</Item>
-          <Item label="State Mismatch Rate">{pfInfo.counters.stateMismatch.rate}/s</Item>
-          <Item label="Total State Inserts">{pfInfo.counters.stateInsert.total}</Item>
-          <Item label="State Insert Rate">{pfInfo.counters.stateInsert.rate}/s</Item>
-          <Item label="Total State Limits">{pfInfo.counters.stateLimit.total}</Item>
-          <Item label="State Limit Rate">{pfInfo.counters.stateLimit.rate}/s</Item>
-          <Item label="Total Source Limits">{pfInfo.counters.srcLimit.total}</Item>
-          <Item label="State Limit Rate">{pfInfo.counters.srcLimit.rate}/s</Item>
-          <Item label="Total SynProxy">{pfInfo.counters.synproxy.total}</Item>
-          <Item label="SynProxy Rate">{pfInfo.counters.synproxy.rate}/s</Item>
-          <Item label="Total Translations">{pfInfo.counters.translate.total}</Item>
-          <Item label="Translation Rate">{pfInfo.counters.translate.rate}/s</Item>
-          <Item label="Total No Routes">{pfInfo.counters.noRoute.total}</Item>
-          <Item label="No Route Rate" span={3}>{pfInfo.counters.noRoute.rate}/s</Item>
-
-          <Item label={<Text strong>Limit Counters</Text>} span={4} />
-          <Item label="Total Max States Per Rule">{pfInfo.limitCounters.maxStatesPerRule.total}</Item>
-          <Item label="Max States Per Rule Rate">{pfInfo.limitCounters.maxStatesPerRule.rate}/s</Item>
-          <Item label="Total Max Source States">{pfInfo.limitCounters.maxSrcStates.total}</Item>
-          <Item label="Max Source States Rate">{pfInfo.limitCounters.maxSrcStates.rate}/s</Item>
-          <Item label="Total Max Source Nodes">{pfInfo.limitCounters.maxSrcNodes.total}</Item>
-          <Item label="Max Source Nodes Rate">{pfInfo.limitCounters.maxSrcNodes.rate}/s</Item>
-          <Item label="Total Max Source Conn">{pfInfo.limitCounters.maxSrcConn.total}</Item>
-          <Item label="Max Source Conn Rate">{pfInfo.limitCounters.maxSrcConn.rate}/s</Item>
-          <Item label="Total Max Source Conn Rate">{pfInfo.limitCounters.maxSrcConnRate.total}</Item>
-          <Item label="Max Source Conn Rate Rate">{pfInfo.limitCounters.maxSrcConnRate.rate}/s</Item>
-          <Item label="Total Overload Table Insersions">{pfInfo.limitCounters.overloadTableInsertion.total}</Item>
-          <Item label="Overload Table Insertion Rate">{pfInfo.limitCounters.overloadTableInsertion.rate}/s</Item>
-          <Item label="Total SYN Floods Detected">{pfInfo.limitCounters.synFloodsDetected.total}</Item>
-          <Item label="SYN Flood Detection Rate">{pfInfo.limitCounters.synFloodsDetected.rate}/s</Item>
-          <Item label="Total SYN Cookies Sent">{pfInfo.limitCounters.syncookiesSent.total}</Item>
-          <Item label="SYN Cookie Send Rate">{pfInfo.limitCounters.syncookiesSent.rate}/s</Item>
-          <Item label="Total SYN Cookies Validated">{pfInfo.limitCounters.syncookiesValidated.total}</Item>
-          <Item label="SYN Cookie Validation Rate" span={3}>{pfInfo.limitCounters.syncookiesValidated.rate}/s</Item>
-
-          <Item label={<Text strong>Adaptive SYN Cookies Watermarks</Text>} span={4} />
-          <Item label="Start">{pfInfo.adaptiveSyncookiesWatermarks.start}</Item>
-          <Item label="End">{pfInfo.adaptiveSyncookiesWatermarks.end}</Item>
-        </Descriptions>
+        <Card title="Counters">
+          <Col span={6}><Statistic title="Total Matches" value={pfInfo.counters.match.total} /></Col>
+          <Col span={6}><Statistic title="Match Rate" value={pfInfo.counters.match.rate} suffix="/s" /></Col>
+          <Col span={6}><Statistic title="Total Bad Offsets" value={pfInfo.counters.badOffsets.total} /></Col>
+          <Col span={6}><Statistic title="Bad Offset Rate" value={pfInfo.counters.badOffsets.rate} suffix="/s" /></Col>
+          <Col span={6}><Statistic title="Total Fragments" value={pfInfo.counters.fragments.total} /></Col>
+          <Col span={6}><Statistic title="Fragment Rate" value={pfInfo.counters.fragments.rate} suffix="/s" /></Col>
+          <Col span={6}><Statistic title="Total Shorts" value={pfInfo.counters.shorts.total} /></Col>
+          <Col span={6}><Statistic title="Short Rate" value={pfInfo.counters.shorts.rate} suffix="/s" /></Col>
+          <Col span={6}><Statistic title="Total Normalizes" value={pfInfo.counters.normalize.total} /></Col>
+          <Col span={6}><Statistic title="Normalize Rate" value={pfInfo.counters.normalize.rate} suffix="/s" /></Col>
+          <Col span={6}><Statistic title="Total Memory" value={pfInfo.counters.match.total} /></Col>
+          <Col span={6}><Statistic title="Memory Rate" value={pfInfo.counters.match.rate} suffix="/s" /></Col>
+          <Col span={6}><Statistic title="Total Bad Timestamps" value={pfInfo.counters.badTimestamp.total} /></Col>
+          <Col span={6}><Statistic title="Bad Timestamp Rate" value={pfInfo.counters.badTimestamp.rate} suffix="/s" /></Col>
+          <Col span={6}><Statistic title="Total Congestion" value={pfInfo.counters.congestion.total} /></Col>
+          <Col span={6}><Statistic title="Congestion Rate" value={pfInfo.counters.congestion.rate} suffix="/s" /></Col>
+          <Col span={6}><Statistic title="Total IP Options" value={pfInfo.counters.ipOption.total} /></Col>
+          <Col span={6}><Statistic title="IP Options Rate" value={pfInfo.counters.ipOption.rate} suffix="/s" /></Col>
+          <Col span={6}><Statistic title="Total Protocol Checksums" value={pfInfo.counters.protoCksum.total} /></Col>
+          <Col span={6}><Statistic title="Protocol Checksum Rate" value={pfInfo.counters.protoCksum.rate} suffix="/s" /></Col>
+          <Col span={6}><Statistic title="Total State Mismatches" value={pfInfo.counters.stateMismatch.total} /></Col>
+          <Col span={6}><Statistic title="State Mismatch Rate" value={pfInfo.counters.stateMismatch.rate} suffix="/s" /></Col>
+          <Col span={6}><Statistic title="Total State Inserts" value={pfInfo.counters.stateInsert.total} /></Col>
+          <Col span={6}><Statistic title="State Insert Rate" value={pfInfo.counters.stateInsert.rate} suffix="/s" /></Col>
+          <Col span={6}><Statistic title="Total State Limits" value={pfInfo.counters.stateLimit.total} /></Col>
+          <Col span={6}><Statistic title="State Limit Rate" value={pfInfo.counters.stateLimit.rate} suffix="/s" /></Col>
+          <Col span={6}><Statistic title="Total Source Limits" value={pfInfo.counters.srcLimit.total} /></Col>
+          <Col span={6}><Statistic title="State Limit Rate" value={pfInfo.counters.srcLimit.rate} suffix="/s" /></Col>
+          <Col span={6}><Statistic title="Total SynProxy" value={pfInfo.counters.synproxy.total} /></Col>
+          <Col span={6}><Statistic title="SynProxy Rate" value={pfInfo.counters.synproxy.rate} suffix="/s" /></Col>
+          <Col span={6}><Statistic title="Total Translations" value={pfInfo.counters.translate.total} /></Col>
+          <Col span={6}><Statistic title="Translation Rate" value={pfInfo.counters.translate.rate} suffix="/s" /></Col>
+          <Col span={6}><Statistic title="Total No Routes" value={pfInfo.counters.noRoute.total} /></Col>
+          <Col span={6}><Statistic title="No Route Rate" span={3} value={pfInfo.counters.noRoute.rate} suffix="/s" /></Col>
+        </Card>
+        <Card title="Limit Counters">
+          <Col span={6}><Statistic title="Total Max States Per Rule" value={pfInfo.limitCounters.maxStatesPerRule.total} /></Col>
+          <Col span={6}><Statistic title="Max States Per Rule Rate" value={pfInfo.limitCounters.maxStatesPerRule.rate} suffix="/s" /></Col>
+          <Col span={6}><Statistic title="Total Max Source States" value={pfInfo.limitCounters.maxSrcStates.total} /></Col>
+          <Col span={6}><Statistic title="Max Source States Rate" value={pfInfo.limitCounters.maxSrcStates.rate} suffix="/s" /></Col>
+          <Col span={6}><Statistic title="Total Max Source Nodes" value={pfInfo.limitCounters.maxSrcNodes.total} /></Col>
+          <Col span={6}><Statistic title="Max Source Nodes Rate" value={pfInfo.limitCounters.maxSrcNodes.rate} suffix="/s" /></Col>
+          <Col span={6}><Statistic title="Total Max Source Conn" value={pfInfo.limitCounters.maxSrcConn.total} /></Col>
+          <Col span={6}><Statistic title="Max Source Conn Rate" value={pfInfo.limitCounters.maxSrcConn.rate} suffix="/s" /></Col>
+          <Col span={6}><Statistic title="Total Max Source Conn Rate" value={pfInfo.limitCounters.maxSrcConnRate.total} /></Col>
+          <Col span={6}><Statistic title="Max Source Conn Rate Rate" value={pfInfo.limitCounters.maxSrcConnRate.rate} suffix="/s" /></Col>
+          <Col span={6}><Statistic title="Total Overload Table Insersions" value={pfInfo.limitCounters.overloadTableInsertion.total} /></Col>
+          <Col span={6}><Statistic title="Overload Table Insertion Rate" value={pfInfo.limitCounters.overloadTableInsertion.rate} suffix="/s" /></Col>
+          <Col span={6}><Statistic title="Total SYN Floods Detected" value={pfInfo.limitCounters.synFloodsDetected.total} /></Col>
+          <Col span={6}><Statistic title="SYN Flood Detection Rate" value={pfInfo.limitCounters.synFloodsDetected.rate} suffix="/s" /></Col>
+          <Col span={6}><Statistic title="Total SYN Cookies Sent" value={pfInfo.limitCounters.syncookiesSent.total} /></Col>
+          <Col span={6}><Statistic title="SYN Cookie Send Rate" value={pfInfo.limitCounters.syncookiesSent.rate} suffix="/s" /></Col>
+          <Col span={6}><Statistic title="Total SYN Cookies Validated" value={pfInfo.limitCounters.syncookiesValidated.total} /></Col>
+          <Col span={6}><Statistic title="SYN Cookie Validation Rate" span={3} value={pfInfo.limitCounters.syncookiesValidated.rate} suffix="/s" /></Col>
+        </Card>
+        <Card title="Adaptive SYN Cookies Watermarks">
+          <Row>
+            <Col span={6}><Statistic title="Start" value={pfInfo.adaptiveSyncookiesWatermarks.start} /></Col>
+            <Col span={6}><Statistic title="End" value={pfInfo.adaptiveSyncookiesWatermarks.end} /></Col>
+          </Row>
+        </Card>
       </Col>
-    </Card>
   );
 }
 
