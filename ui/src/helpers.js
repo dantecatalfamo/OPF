ce9da7699d6f51dab930d2dae8a2efd6f4ed85af
Dante Catalfamo <dante.catalfamo@gmail.com>
2020-01-07 23:07:17 -0500

    postJSON helper

--- ui/src/helpers.js
+++ ui/src/helpers.js
@@ -2,3 +2,11 @@ export async function getJSON(url) {
   const response = await fetch(url);
   return await response.json();
 }
+
+export async function postJSON(url, data) {
+  const response = await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(data),
+  });
+  return await response.json();
+}
