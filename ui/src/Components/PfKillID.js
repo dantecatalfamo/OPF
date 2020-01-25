48498d7b5b3243740a755732dcf3565fe70329d6
Dante Catalfamo <dante.catalfamo@gmail.com>
2020-01-25 15:11:23 -0500

    Add pf kill by id component

---
+++ ui/src/Components/PfKillID.js
@@ -0,0 +1,36 @@
+import React, { useState, useCallback } from 'react';
+import { Button, message } from 'antd';
+import { postJSON } from '../helpers.js';
+import { serverURL } from '../config.js';
+
+const pfKillIdURL = `${serverURL}/api/pf-kill-id`;
+
+function PfKillID(props) {
+  const id = props.id;
+  const [submitting, setSubmitting] = useState(false);
+  const [disabled, setDisabled] = useState(false);
+
+  const handleClick = useCallback(() => {
+    setSubmitting(true);
+    postJSON(pfKillIdURL, id)
+      .then(res => {
+        setSubmitting(false);
+        setDisabled(true); // disbable button until states update and row is removed
+        message.success(`Killed state ${id}`);
+      }).catch (res => {
+        message.error(`Failed to kill state with ID ${id}`);
+        setSubmitting(false);
+      });
+  }, [id]);
+
+  return (
+    <Button
+      size="small"
+      disabled={disabled}
+      loading={submitting}
+      onClick={handleClick}
+    >Kill</Button>
+  );
+}
+
+export default PfKillID;
