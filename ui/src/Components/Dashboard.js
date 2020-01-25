779467d9df3b8268610af0582783f66e8c9e92ef
Dante Catalfamo <dante.catalfamo@gmail.com>
2020-01-25 17:46:27 -0500

    Begun work on dashboard page

--- ui/src/Components/Dashboard.js
+++ ui/src/Components/Dashboard.js
@@ -1,8 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card } from 'antd';
+import { getJSON, useJsonUpdates } from '../helpers';
+import { serverURL } from '../config';
+
+const unameURL = `${serverURL}/api/uname`;
+const updateTime = 5000;
+
+function Uname(props) {
+  const [uname, setUname] = useState();
+
+  useJsonUpdates(unameURL, setUname, updateTime);
+
+  return (
+    <Card title={uname ? uname.nodeName : ""} style={{width: 300, margin: 12}}>
+      <p>OS Name: {uname ? `${uname.osName} ${uname.osRelease}` : ""}</p>
+      <p>Platform: {uname ? `${uname.hardware}` : ""}</p>
+    </Card>
+  );
+}
 
 function Dashboard(props) {
-  return (<Card>Hello!</Card>);
+  return (
+    <div>
+      <Uname/>
+    </div>
+  );
 }
 
 export default Dashboard;
