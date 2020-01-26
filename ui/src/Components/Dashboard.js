b017ea47dba73b163d3d7187b3c537b73b099c2c
Dante Catalfamo <dante.catalfamo@gmail.com>
2020-01-26 17:54:38 -0500

    Better looking ram tooltip

--- ui/src/Components/Dashboard.js
+++ ui/src/Components/Dashboard.js
@@ -56,7 +56,7 @@ function Ram(props) {
 
   return (
     <Card title="RAM">
-      <Tooltip title={`${active}M Active+${other}M Other / ${total}M Total`}>
+      <Tooltip title={<div style={{textAlign: "center"}}>{active}MB Active + {other}MB Other <br/> / {total}M Total</div>}>
         <Progress percent={percentUsed} successPercent={percentActive} type="dashboard"/>
       </Tooltip>
     </Card>
