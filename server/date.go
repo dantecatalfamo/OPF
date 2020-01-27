c6e4cd28b0eeaf20d9ffcaf43dc6d66ba35707db
Dante Catalfamo <dante.catalfamo@gmail.com>
2020-01-27 18:25:02 -0500

    Make trimming string more obvious

--- server/date.go
+++ server/date.go
@@ -1,12 +1,16 @@
 package main
 
-import "os/exec"
+import (
+	"os/exec"
+	"strings"
+)
 
 func GetDate() (string, error) {
 	outBytes, err := exec.Command("date").Output()
 	if err != nil {
 		return "", err
 	}
-	output := string(outBytes[:len(outBytes)-1]) // remove newline
+	output := string(outBytes)
+	output = strings.TrimRight(output, "\n")
 	return output, nil
 }
