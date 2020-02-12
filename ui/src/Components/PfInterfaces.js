e8bac6beb50208d7224dfcf08fac403b2962f529
Dante Catalfamo <dante.catalfamo@gmail.com>
2020-02-11 23:06:10 -0500

    Rearrange functions

--- ui/src/Components/PfInterfaces.js
+++ ui/src/Components/PfInterfaces.js
@@ -10,100 +10,6 @@ const pfInterfacesURL = `${serverURL}/api/pf-interfaces`;
 const updateTime = 3000;
 const diffTime = updateTime / 1000;
 
-function PfInterfaces(props) {
-  const [interfaces, updateInterfaces] = useReducer((state, update) => {
-    let ifaces = {};
-    update.forEach(iface => {
-      const name = iface.interface;
-      ifaces[name] = iface;
-      const newPass4In = iface.in4pass.bytes;
-      const newPass4Out = iface.out4pass.bytes;
-      const newPass6In = iface.in6pass.bytes;
-      const newPass6Out = iface.out6pass.bytes;
-      const newBlock4In = iface.in4block.bytes;
-      const newBlock4Out = iface.out4block.bytes;
-      const newBlock6In = iface.in6block.bytes;
-      const newBlock6Out = iface.out6block.bytes;
-      let oldPass4In;
-      let oldPass4Out;
-      let oldPass6In;
-      let oldPass6Out;
-      let oldBlock4In;
-      let oldBlock4Out;
-      let oldBlock6In;
-      let oldBlock6Out;
-      let oldHistory;
-      if (!state || state === {} || !state[name]) {
-        oldPass4In = newPass4In;
-        oldPass4Out = newPass4Out;
-        oldPass6In = newPass6In;
-        oldPass6Out = newPass6Out;
-        oldBlock4In = newBlock4In;
-        oldBlock4Out = newBlock4Out;
-        oldBlock6In = newBlock6In;
-        oldBlock6Out = newBlock6Out;
-        oldHistory = [];
-      } else {
-        oldPass4In = state[name].in4pass.bytes;
-        oldPass4Out = state[name].out4pass.bytes;
-        oldPass6In = state[name].in6pass.bytes;
-        oldPass6Out = state[name].out6pass.bytes;
-        oldBlock4In = state[name].in4block.bytes;
-        oldBlock4Out = state[name].out4block.bytes;
-        oldBlock6In = state[name].in6block.bytes;
-        oldBlock6Out = state[name].out6block.bytes;
-        oldHistory = state[name].history;
-      }
-      const diffPass4In = Number(((newPass4In - oldPass4In) / diffTime / 1024 / 1024).toFixed(2));
-      const diffPass4Out = Number((-(newPass4Out - oldPass4Out) / diffTime / 1024 / 1024).toFixed(2));
-      const diffPass6In = Number(((newPass6In - oldPass6In) / diffTime / 1024 / 1024).toFixed(2));
-      const diffPass6Out = Number((-(newPass6Out - oldPass6Out) / diffTime / 1024 / 1024).toFixed(2));
-      const diffBlock4In = Number(((newBlock4In - oldBlock4In) / diffTime / 1024 / 1024).toFixed(2));
-      const diffBlock4Out = Number(((newBlock4Out - oldBlock4Out) / diffTime / 1024 / 1024).toFixed(2));
-      const diffBlock6In = Number(((newBlock6In - oldBlock6In) / diffTime / 1024 / 1024).toFixed(2));
-      const diffBlock6Out = Number(((newBlock6Out - oldBlock6Out) / diffTime / 1024 / 1024).toFixed(2));
-      const date = new Date();
-      const time = date; // `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`; // date;
-      const point = {
-        time: time,
-        pass4in: diffPass4In,
-        pass4out: diffPass4Out,
-        pass6in: diffPass6In,
-        pass6out: diffPass6Out,
-        block4in: diffBlock4In,
-        block4out: diffBlock4Out,
-        block6in: diffBlock6In,
-        block6out: diffBlock6Out,
-      };
-
-      if (oldHistory.length > 60) {
-        oldHistory.shift();
-      }
-
-      ifaces[name].history = [...oldHistory, point];
-    });
-    return ifaces;
-  }, {});
-
-  useJsonUpdates(pfInterfacesURL, updateInterfaces, updateTime);
-
-  if (Object.keys(interfaces).length == 0) {
-    return (
-      <Spin>
-        <Card style={{margin: "30px"}}></Card>
-      </Spin>);
-  }
-
-  return (
-    <div style={{marginBottom: "12px"}}>
-      {Object.keys(interfaces).map(name => {
-        const iface = interfaces[name];
-        return (<PfInterface iface={iface} />);
-      })}
-    </div>
-  );
-}
-
 function PfInterface(props) {
   const iface = props.iface;
 
@@ -253,6 +159,100 @@ function PfInterface(props) {
         {(ipv4 || ipv6) ? graph : ""}
       </Card>
     </Col>
+  );
+}
+
+function PfInterfaces(props) {
+  const [interfaces, updateInterfaces] = useReducer((state, update) => {
+    let ifaces = {};
+    update.forEach(iface => {
+      const name = iface.interface;
+      ifaces[name] = iface;
+      const newPass4In = iface.in4pass.bytes;
+      const newPass4Out = iface.out4pass.bytes;
+      const newPass6In = iface.in6pass.bytes;
+      const newPass6Out = iface.out6pass.bytes;
+      const newBlock4In = iface.in4block.bytes;
+      const newBlock4Out = iface.out4block.bytes;
+      const newBlock6In = iface.in6block.bytes;
+      const newBlock6Out = iface.out6block.bytes;
+      let oldPass4In;
+      let oldPass4Out;
+      let oldPass6In;
+      let oldPass6Out;
+      let oldBlock4In;
+      let oldBlock4Out;
+      let oldBlock6In;
+      let oldBlock6Out;
+      let oldHistory;
+      if (!state || state === {} || !state[name]) {
+        oldPass4In = newPass4In;
+        oldPass4Out = newPass4Out;
+        oldPass6In = newPass6In;
+        oldPass6Out = newPass6Out;
+        oldBlock4In = newBlock4In;
+        oldBlock4Out = newBlock4Out;
+        oldBlock6In = newBlock6In;
+        oldBlock6Out = newBlock6Out;
+        oldHistory = [];
+      } else {
+        oldPass4In = state[name].in4pass.bytes;
+        oldPass4Out = state[name].out4pass.bytes;
+        oldPass6In = state[name].in6pass.bytes;
+        oldPass6Out = state[name].out6pass.bytes;
+        oldBlock4In = state[name].in4block.bytes;
+        oldBlock4Out = state[name].out4block.bytes;
+        oldBlock6In = state[name].in6block.bytes;
+        oldBlock6Out = state[name].out6block.bytes;
+        oldHistory = state[name].history;
+      }
+      const diffPass4In = Number(((newPass4In - oldPass4In) / diffTime / 1024 / 1024).toFixed(2));
+      const diffPass4Out = Number((-(newPass4Out - oldPass4Out) / diffTime / 1024 / 1024).toFixed(2));
+      const diffPass6In = Number(((newPass6In - oldPass6In) / diffTime / 1024 / 1024).toFixed(2));
+      const diffPass6Out = Number((-(newPass6Out - oldPass6Out) / diffTime / 1024 / 1024).toFixed(2));
+      const diffBlock4In = Number(((newBlock4In - oldBlock4In) / diffTime / 1024 / 1024).toFixed(2));
+      const diffBlock4Out = Number(((newBlock4Out - oldBlock4Out) / diffTime / 1024 / 1024).toFixed(2));
+      const diffBlock6In = Number(((newBlock6In - oldBlock6In) / diffTime / 1024 / 1024).toFixed(2));
+      const diffBlock6Out = Number(((newBlock6Out - oldBlock6Out) / diffTime / 1024 / 1024).toFixed(2));
+      const date = new Date();
+      const time = date; // `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`; // date;
+      const point = {
+        time: time,
+        pass4in: diffPass4In,
+        pass4out: diffPass4Out,
+        pass6in: diffPass6In,
+        pass6out: diffPass6Out,
+        block4in: diffBlock4In,
+        block4out: diffBlock4Out,
+        block6in: diffBlock6In,
+        block6out: diffBlock6Out,
+      };
+
+      if (oldHistory.length > 60) {
+        oldHistory.shift();
+      }
+
+      ifaces[name].history = [...oldHistory, point];
+    });
+    return ifaces;
+  }, {});
+
+  useJsonUpdates(pfInterfacesURL, updateInterfaces, updateTime);
+
+  if (Object.keys(interfaces).length == 0) {
+    return (
+      <Spin>
+        <Card style={{margin: "30px"}}></Card>
+      </Spin>);
+  }
+
+  return (
+    <div style={{marginBottom: "12px"}}>
+      {Object.keys(interfaces).map(name => {
+        const iface = interfaces[name];
+        return (<PfInterface iface={iface} />);
+      })}
+    </div>
   );
 }
 
