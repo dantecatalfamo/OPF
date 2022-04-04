ac6adef0cc07beb30e6bbed80b429d312900a119
Dante Catalfamo <dante.catalfamo@gmail.com>
2022-04-03 22:21:12 -0400

    Run app in react 18 mode

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { createRoot } from 'react-dom/client';
 import App from './Components/App';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
