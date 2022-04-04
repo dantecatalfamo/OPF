f5003738798aa29191cf4493c3fa71a0bcaf4772
Dante Catalfamo <dante.catalfamo@gmail.com>
2022-04-04 00:57:47 -0400

    Show json in skeleton wireguard page

--- ui/src/Components/Wireguard.tsx
+++ ui/src/Components/Wireguard.tsx
@@ -1,11 +1,14 @@
-import React, {useState} from 'react';
+import { useState } from 'react';
 import { serverURL } from '../config';
+import { useJsonUpdates } from '../helpers';
 
-const wireguardURL = `${serverURL}/api/wireguard`;
+const wireguardURL = `${serverURL}/api/wireguard-interfaces`;
 
-interface WireguardProps {
-}
+/* interface WireguardProps {
+ * } */
 
-export default function Wireguard (props: WireguardProps) {
-  return wireguardURL
+export default function Wireguard() {
+  const [wgData, setWgData] = useState({});
+  useJsonUpdates(wireguardURL, setWgData, 5 * 1000);
+  return JSON.stringify(wgData);
 }
