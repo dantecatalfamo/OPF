ac97eeacb6db7524b6adf4b9bc665fbab908ca66
Dante Catalfamo <dante.catalfamo@gmail.com>
2022-04-05 01:10:53 -0400

    Improve wireguard layout slightly

--- ui/src/Components/Wireguard.tsx
+++ ui/src/Components/Wireguard.tsx
@@ -1,13 +1,15 @@
 import {
-  Badge, Card, Col, Descriptions, Divider, Row,
+  Badge, Card, Col, Descriptions, Divider, Row, Grid,
 } from 'antd';
 import React, { useState } from 'react';
 import { serverURL } from '../config';
 import { useJsonUpdates } from '../helpers';
 
+const { useBreakpoint } = Grid;
 const wireguardURL = `${serverURL}/api/wireguard-interfaces`;
 
 export default function Wireguard() {
+  const breakpoint = useBreakpoint();
   const [wgInterfaces, setWgInterfaces] = useState<any[]>([]);
   useJsonUpdates(wireguardURL, setWgInterfaces, 5 * 1000);
   const ifaces = wgInterfaces.map((iface) => {
@@ -15,17 +17,15 @@ export default function Wireguard() {
     const isUp = flags.includes('UP');
     const addrs = iface.addresses.map((addr: any) => `${addr.address}/${addr.netmask}`).join(', ');
     const peers = iface.peers.map((peer: any) => (
-      <Col span={24} xl={{ span: 12 }}>
-        <Card title={peer.publicKey} type="inner">
-          <Descriptions size="small" column={4}>
-            <Descriptions.Item span={2} label="Endpoint">{peer.endpointAddress ? `${peer.endpointAddress}:${peer.endpointPort}` : '<Not connected>'}</Descriptions.Item>
-            <Descriptions.Item span={2} label="Last Handshake">{`${peer.lastHandshake} seconds`}</Descriptions.Item>
-            <Descriptions.Item span={2} label="Transmitted">{`${peer.transmitted} bytes`}</Descriptions.Item>
-            <Descriptions.Item span={2} label="Received">{`${peer.received} bytes`}</Descriptions.Item>
-            <Descriptions.Item span={4} label="Allowed IPs">{peer.allowedIPs.join(', ')}</Descriptions.Item>
-          </Descriptions>
-        </Card>
-      </Col>
+      <Card.Grid style={{ width: breakpoint.lg ? '50%' : '100%' }}>
+        <Descriptions title={peer.publicKey} size="small" column={4}>
+          <Descriptions.Item span={2} label="Endpoint">{peer.endpointAddress ? `${peer.endpointAddress}:${peer.endpointPort}` : '<Not connected>'}</Descriptions.Item>
+          <Descriptions.Item span={2} label="Last Handshake">{`${peer.lastHandshake} seconds`}</Descriptions.Item>
+          <Descriptions.Item span={2} label="Transmitted">{`${peer.transmitted} bytes`}</Descriptions.Item>
+          <Descriptions.Item span={2} label="Received">{`${peer.received} bytes`}</Descriptions.Item>
+          <Descriptions.Item span={4} label="Allowed IPs">{peer.allowedIPs.join(', ')}</Descriptions.Item>
+        </Descriptions>
+      </Card.Grid>
     ));
 
     return (
@@ -36,14 +36,16 @@ export default function Wireguard() {
         </span>
       )}
       >
-        <Descriptions column={4}>
+        <Descriptions bordered column={4} size="small">
           <Descriptions.Item span={2} label="Public Key">{iface.publicKey}</Descriptions.Item>
           <Descriptions.Item span={1} label="Addresses">{addrs}</Descriptions.Item>
           <Descriptions.Item span={1} label="Port">{iface.port}</Descriptions.Item>
         </Descriptions>
         <Divider plain>Clients</Divider>
         <Row gutter={[4, 4]}>
-          {peers}
+          <Col span={24}>
+            {peers}
+          </Col>
         </Row>
       </Card>
     );
