ffca3517199d99ea16ca20f2e349d3a60fa0f82e
Dante Catalfamo <dante.catalfamo@gmail.com>
2022-04-07 21:20:33 -0400

    Render in React 17 mode until antd is fixed

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+// import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './Components/App';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<App />);
+ReactDOM.render(<App />, container);
+
+// Disable react 18 mode until antd is fixed
+// https://github.com/ant-design/ant-design/issues/34890
+// https://github.com/ant-design/ant-design/projects/7#card-80292346=
+// const root = createRoot(container);
+// root.render(<App />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
